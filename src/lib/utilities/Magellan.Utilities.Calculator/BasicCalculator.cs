302457e4901e96f5d40bef1064d64c42349fbbac
ettienemare <ettienemare@gmail.com>
2023-02-27 06:21:52 +0200

    Update the Unit tests

--- src/lib/utilities/Magellan.Utilities.Calculator/BasicCalculator.cs
+++ src/lib/utilities/Magellan.Utilities.Calculator/BasicCalculator.cs
@@ -4,4 +4,8 @@ public sealed class BasicCalculator {
   public int Add(int a, int b) => a + b;
 
   public int Subtract(int a, int b) => a - b;
+
+  public int Multiply(int a, int b) => throw new NotImplementedException();
+  
+  public int Divide(int a, int b) => throw new NotImplementedException();
 }
--- test/lib/utilities/Magellan.Utilities.Calculator.Tests.Unit/BasicCalculatorTests.cs
+++ test/lib/utilities/Magellan.Utilities.Calculator.Tests.Unit/BasicCalculatorTests.cs
@@ -27,18 +27,42 @@ public sealed class BasicCalculatorTests: IDisposable {
     _outputHelper.WriteLine("Sum Test Executed");
   }
   
-  [Fact]
-  public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers() {
-    // Arrange
-    const int firstNumber = 10;
-    const int secondNumber = 2;
-    const int differenceOfFirstAnsSecondNumber = firstNumber - secondNumber;
-    
+  [Theory]
+  [InlineData(10, 2, 8)]
+  [InlineData(17, 8, 9)]
+  public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(int firstNumber, int secondNumber, int differenceOfFirstAnsSecondNumber) {
     // Act
     var result = _sut.Subtract(firstNumber, secondNumber);
 
     // Assert
     result.Should().Be(differenceOfFirstAnsSecondNumber);
+    
+    _outputHelper.WriteLine("Subtract Test Executed");
+  }
+  
+  [Fact(Skip = "Not implemented yet")]
+  public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntegers() {
+    // Arrange
+    const int firstNumber = 5;
+    const int secondNumber = 8;
+    const int productOfFirstAnsSecondNumber = firstNumber + secondNumber;
+    
+    // Act
+    var result = _sut.Multiply(firstNumber, secondNumber);
+
+    // Assert
+    result.Should().Be(productOfFirstAnsSecondNumber);
+  }
+  
+  [Theory(Skip = "Breaks integration")]
+  [InlineData(10, 2, 8, Skip = "Breaks Integration")]
+  [InlineData(17, 8, 9)]
+  public void Divide_ShouldDivideTwoNumbers_WhenTwoNumbersAreIntegers(int firstNumber, int secondNumber, int quotientOfFirstAnsSecondNumber) {
+    // Act
+    var result = _sut.Subtract(firstNumber, secondNumber);
+
+    // Assert
+    result.Should().Be(quotientOfFirstAnsSecondNumber);
     
     _outputHelper.WriteLine("Subtract Test Executed");
   }
